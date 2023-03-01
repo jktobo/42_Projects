2113eec33bc69bce3bce5bc50d284bb930abb5a5
jktobo <joldosh.k@gmail.com>
2023-03-01 16:06:59 +0400

    add structure and fixed

--- Philosopher/philo.c
+++ Philosopher/philo.c
@@ -6,20 +6,21 @@
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 11:29:49 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/02/27 17:29:11 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/03/01 15:46:59 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <string.h>
 
-pthread_mutex_t forks[NUM_FORKS];
-pthread_t ph[NUM_PHILOSOPHERS];
+// pthread_mutex_t forks[NUM_FORKS];
+// pthread_t ph[NUM_PHILOSOPHERS];
 
 void    ft_init_str(t_rules *st, char **av, int ac)
 {
-    int philos;
+    // int philos;
 
-    philos = ft_atoi(av[1]);
+    // philos = ft_atoi(av[1]);
     st->philosophers = ft_atoi(av[1]);
     st->time_die = ft_atoi(av[2]);
     st->time_eat = ft_atoi(av[3]);
@@ -28,78 +29,80 @@ void    ft_init_str(t_rules *st, char **av, int ac)
         st->opt_arg = ft_atoi(av[5]);
     else
         st->opt_arg = -1;
-    st->ph[philos];
-    st->forks[];
+
 }
 
 void    *philo(void *args)
 {
-    int id = *(int*)args;
-    int l_fork = id;
-    int r_fork = (id + 1) % NUM_FORKS;
+    t_philo philo = *(t_philo*)args;
+    philo.right_fork = philo.id;
+    philo.left_fork = (philo.id + 1) % philo.test2->philosophers;
+
 
     while (1)
     {
-        printf("Philo %d is thinking\n", id);
+        printf("Philo %d is thinking\n", philo.id);
+        if ( philo.test2->arr[philo.id] == 0 && philo.test2->arr[(philo.id + 1) % philo.test2->philosophers] == 0)
+        {
+            printf("id = %d l_fork = %d r_fork = %d\n", philo.id, philo.right_fork, philo.left_fork);
+            // pthread_mutex_lock(&philo.test2->forks[philo.right_fork]);
+            // philo.test2->arr[philo.id] = 1;
+            // printf("Philo %d has taken a right fork %d\n", philo.id, philo.right_fork);
+            
+            // pthread_mutex_lock(&philo.test2->forks[philo.left_fork]);
+            // printf("Philo %d has taken a left fork %d\n", philo.id, philo.left_fork);
+            // philo.test2->arr[(philo.id + 1) % philo.test2->philosophers] = 1;
 
-        pthread_mutex_lock(&forks[l_fork]);
-        printf("Philo %d has taken a left fork %d\n", id, l_fork);
-        pthread_mutex_lock(&forks[r_fork]);
-        printf("Philo %d has taken a right fork %d\n", id, r_fork);
-        pthread_mutex_unlock(&forks[r_fork]);        
-        pthread_mutex_unlock(&forks[l_fork]);
-        usleep(10000);
+            // pthread_mutex_unlock(&philo.test2->forks[philo.right_fork]);
+            // philo.test2->arr[(philo.id + 1) % philo.test2->philosophers] = 0;       
+            // pthread_mutex_unlock(&philo.test2->forks[philo.left_fork]);
+            // philo.test2->arr[philo.id] = 0;
+            // ft_my_sleep(100);
+        }
     }
     return (NULL);
 }
 
+// int main()
 int main(int ac, char **av)
 {
     int i;
-    t_rules *rules = NULL;
+    t_rules rules;
 
-    ft_checker(ac, av);
-    ft_init_str(rules, av, ac);
+    memset(&rules, 0, sizeof(t_rules));
+    // rules->start_time = get_time();
+    // ft_checker(ac, av);
+    ft_init_str(&rules, av, ac);
+    rules.ph = calloc(sizeof(pthread_t), rules.philosophers + 1);
+    rules.forks = calloc(sizeof(pthread_mutex_t), rules.philosophers + 1);
+    rules.philo = calloc(sizeof(t_philo), rules.philosophers + 1);
+    rules.arr = calloc(sizeof(int), rules.philosophers);
     i = -1;
-    while (++i < rules->philosophers)
-        pthread_mutex_init(&forks[i], NULL);
+    while (++i < rules.philosophers)
+        pthread_mutex_init(&rules.forks[i], NULL);
+    
     i = -1;
-    while (++i < rules->philosophers)
-    {
-        pthread_create(&ph[i], NULL, &philo, &i);
+    while (++i < rules.philosophers) {
+        rules.philo[i].id = i;
+        rules.philo[i].test2 = &rules;
+        pthread_create(&rules.ph[i], NULL, &philo, &rules.philo[i]);
     }
     i = -1;
-    while (++i < rules->philosophers)
-    {
-        pthread_join(ph[i], NULL);
-    }
+    while (++i < rules.philosophers)
+        pthread_join(rules.ph[i], NULL);
     i = -1;
-    while (++i < rules->philosophers)
-        pthread_mutex_destroy(&forks[i]);
+    while (++i < rules.philosophers)
+        pthread_mutex_destroy(&rules.forks[i]);
     return (0);
 }
-// int main(int ac, char **av)
-// {
-//     int i;
-//     // t_philo *st = NULL;
-//     t_rules *rules = NULL;
-//     pthread_t pt[ft_atoi(av[1])];
 
-//     ft_checker(ac, av);
-//     rules = malloc(sizeof(t_rules) * ft_atoi(av[1]));
-//     ft_init_str(rules, av, ac);
-//     i = -1;
-//     while (++i < ft_atoi(av[1]))
-//     {
-//         pthread_create(&pt[i], NULL, &philo, &i) ;
-//     }
-//     i = -1;
-//     while (++i < ft_atoi(av[1]))
-//     {
-//         pthread_join(pt[i], NULL);
-//     }
-//     // pthread_mutex_destroy(&mutex);
-//     return (0);
-// }
+// FORK STATE
+// { 0, 0, 0, 0, 0 }
 
+// MUTEX_LOCK
+// UPDATE
+// { 1, 1, 0, 0, 0 }
+// UNLOCK
+
+// if 2 checks
 
--- Philosopher/philo.h
+++ Philosopher/philo.h
@@ -6,7 +6,7 @@
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 16:17:05 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/02/27 18:17:46 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/03/01 15:35:23 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 #include <stdlib.h>
 
 struct s_rules;
-#define NUM_PHILOSOPHERS 5
-#define NUM_FORKS 5
+#define NUM_PHILOSOPHERS 10
+#define NUM_FORKS 10
 
 typedef	struct s_philo
 {
@@ -26,35 +26,26 @@ typedef	struct s_philo
 	int				last_time_eat;
 	int				left_fork;
 	int				right_fork;
-	pthread_mutex_t	r_mutfork;
-	pthread_mutex_t	l_mutfork;
 	struct s_rules	*test2;
 } t_philo;
 
 typedef	struct s_rules
 {
 	// pthread_mutex_t	mutex;
+	int				id;
 	int				philosophers;
 	int				time_die;
 	int				time_eat;
 	int				time_sleep;
 	int				opt_arg;
-	pthread_t		ph[250];
-	pthread_mutex_t forks[250];
+	long			start_time;
+	int				*arr;
+	pthread_t		*ph;
+	pthread_mutex_t *forks;
+	t_philo			*philo;
+	// pthread_t		ph[250];
+	// pthread_mutex_t forks[250];
 } t_rules;
-
-// typedef	struct s_rules
-// {
-// 	// pthread_mutex_t	mutex;
-// 	int				philosophers;
-// 	int				time_die;
-// 	int				time_eat;
-// 	int				time_sleep;
-// 	int				opt_arg;
-// 	t_philo			philo[250];
-// 	int				fork[250];
-// 	pthread_mutex_t mutex[250];
-// } t_rules;
 
 void    ft_error(void);
 int		ft_check_digit(char **av); 
