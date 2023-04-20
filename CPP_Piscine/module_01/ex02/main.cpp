9f01331b33df790afa3d911ee0653f704976e231
jktobo <joldosh.k@gmail.com>
2023-04-20 18:02:12 +0400

    add all folder and files of modul 01

--- CPP_Piscine/module_01/ex02/main.cpp
+++ CPP_Piscine/module_01/ex02/main.cpp
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/20 16:54:32 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/04/20 16:54:34 by dkaratae         ###   ########.fr       */
+/*   Created: 2023/04/20 16:57:57 by dkaratae          #+#    #+#             */
+/*   Updated: 2023/04/20 16:57:58 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef     ZOMBIE_HPP
-#define     ZOMBIE_HPP
-
-#include <string>
-#include <iostream>
-
-class Zombie
-{
-    private:
-        std::string name;
-    public:
-        Zombie();
-        Zombie(std::string);
-        ~Zombie();
-        void announce(void);
-};
-Zombie *newZombie(std::string name);
-void randomChump(std::string name);
-#endif
--- CPP_Piscine/module_01/ex03/Weapon.hpp
+++ CPP_Piscine/module_01/ex03/Weapon.hpp
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/20 16:54:32 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/04/20 16:54:34 by dkaratae         ###   ########.fr       */
+/*   Created: 2023/04/20 16:59:45 by dkaratae          #+#    #+#             */
+/*   Updated: 2023/04/20 16:59:46 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef     ZOMBIE_HPP
-#define     ZOMBIE_HPP
-
-#include <string>
-#include <iostream>
-
-class Zombie
-{
-    private:
-        std::string name;
-    public:
-        Zombie();
-        Zombie(std::string);
-        ~Zombie();
-        void announce(void);
-};
-Zombie *newZombie(std::string name);
-void randomChump(std::string name);
-#endif
