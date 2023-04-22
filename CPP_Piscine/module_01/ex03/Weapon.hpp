e35c405e6314f3ad09debf6c174336bde585e2ee
jktobo <joldosh.k@gmail.com>
2023-04-22 18:02:55 +0400

    start mod-01 ex03

--- CPP_Piscine/module_01/ex03/Weapon.hpp
+++ CPP_Piscine/module_01/ex03/Weapon.hpp
@@ -6,7 +6,36 @@
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 16:59:45 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/04/20 16:59:46 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/04/22 16:52:13 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+class Weapon
+{
+	private:
+		std::string type;
+	public:
+		Weapon::Weapon()
+		{
+			
+		}
+		
+		Weapon::~Weapon()
+		{
+			
+		}
+		
+		const std::string &getType()
+		{
+			return (type);
+		}
+		
+		void setType(std::string a_type)
+		{
+			type = a_type;
+		}
+		
+}
