3df81bc102e68f11fc01d4717d2390ceed2d9e26
Joldosh <joldosh.k@gmail.com>
2023-04-24 15:47:40 +0400

    mod-01 ex-04

--- CPP_Piscine/module_01/ex01/zombieHorde.cpp
+++ CPP_Piscine/module_01/ex01/zombieHorde.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*   By: joldosh <joldosh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 16:57:05 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/04/21 11:38:09 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/04/24 13:57:27 by joldosh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie* zombieHorde(int n, std::string name)
 {
 	Zombie *zombie = new Zombie[n];
 	
-	for (int i = 0; i <= N; i++)
+	for (int i = 0; i <= n; i++)
 	{
 		zombie[i].setName(name);
 		zombie[i].announce();
--- CPP_Piscine/module_01/ex04/main.cpp
+++ CPP_Piscine/module_01/ex04/main.cpp
@@ -3,10 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*   By: joldosh <joldosh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:03:50 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/04/20 17:03:51 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/04/24 13:40:09 by joldosh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        std::cout << "Error! You should to enter 1 - file name, 2 - string1 and 3 - string2" << std::endl;
+        return (1);
+    }
+
+    
+    return (0);
+}
