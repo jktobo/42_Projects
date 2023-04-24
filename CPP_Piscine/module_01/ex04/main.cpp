c1eec3a28d9566781ff5a18a282380717767aa33
jktobo <joldosh.k@gmail.com>
2023-04-24 19:23:55 +0400

    add some function in mod 01 ex04

--- CPP_Piscine/module_01/ex04/main.cpp
+++ CPP_Piscine/module_01/ex04/main.cpp
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joldosh <joldosh@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:03:50 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/04/24 13:40:09 by joldosh          ###   ########.fr       */
+/*   Updated: 2023/04/24 18:41:55 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc, char **argv)
@@ -20,7 +21,25 @@ int main(int argc, char **argv)
         std::cout << "Error! You should to enter 1 - file name, 2 - string1 and 3 - string2" << std::endl;
         return (1);
     }
-
+    std::ifstream infile(argv[1]);
+    if (!(infile.is_open()))
+    {
+        std::cout << "You should include file!" << std::endl;
+    }
+    std::string str;
+    size_t pos = 0;
+    std::getline(infile, str, '\0');
+    int len = str.length();
+ 
+    std::string str3 = argv[3];
+    while ((pos = str.find(argv[2])) != std::string::npos)
+    {
+        std::cout << "find" << std::endl;
+        std::cout << pos << std::endl;
+        pos += str3.length();
+    }
+    // std::cout << str << std::endl;
+    
     
     return (0);
 }
