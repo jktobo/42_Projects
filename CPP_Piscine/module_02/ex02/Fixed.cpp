21a1628298df90bd2c10c0b0338254955d6f850f
jktobo <joldosh.k@gmail.com>
2023-05-08 16:06:27 +0400

    start mod02 ex02

--- CPP_Piscine/module_02/ex02/Fixed.cpp
+++ CPP_Piscine/module_02/ex02/Fixed.cpp
@@ -6,7 +6,72 @@
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:46:32 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/05/03 17:46:33 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/05/08 16:05:59 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    std::cout << "Default constructor called" << std::endl;
+    fixed_point = 0;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::Fixed(const int intNum)
+{
+    std::cout << "Int constructor called" << std::endl;
+    fixed_point = intNum << FB;
+}
+
+Fixed::Fixed(const float floatNum)
+{
+    std::cout << "Float constructor called" << std::endl;
+    fixed_point = int(roundf(floatNum * (1 << FB)));
+}
+
+Fixed &Fixed::operator = (const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->fixed_point = other.fixed_point;
+    return *this;
+}
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return fixed_point;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    fixed_point = raw;
+}
+
+
+float Fixed::toFloat(void) const
+{
+    return float(fixed_point) / float(1 << FB); 
+}
+
+int Fixed::toInt( void ) const
+{
+    return (fixed_point >> FB);
+}
+
+std::ostream& operator<<(std::ostream &stream, const Fixed &print)
+{
+    stream << print.toFloat();
+    return stream;
+}
--- CPP_Piscine/module_02/ex02/Fixed.hpp
+++ CPP_Piscine/module_02/ex02/Fixed.hpp
@@ -6,7 +6,36 @@
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:46:34 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/05/03 17:46:35 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/05/08 16:06:11 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int fixed_point;
+        static const int FB = 8;
+    public:
+        Fixed();
+        Fixed(const Fixed &other);
+        Fixed(const int intNum);
+        Fixed(const float floatNum);
+        Fixed &operator = (const Fixed &other);
+        
+        ~Fixed();
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+
+        float toFloat(void) const;
+        int toInt(void) const;
+};
+std::ostream& operator<<(std::ostream &os, const Fixed &print);
+#endif
