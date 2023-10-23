58b24cf336e8bf8299321cbdc02cc3fdace9c3dc
jktobo <joldosh.k@gmail.com>
2023-10-23 17:12:27 +0400

    Start Module 5 and excrsize 00

--- CPP_Piscine/module_05/ex00/Bureaucrat.cpp
+++ CPP_Piscine/module_05/ex00/Bureaucrat.cpp
@@ -6,7 +6,37 @@
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 18:42:24 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/06/13 18:42:25 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/10/23 16:42:49 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+#include <exception>
+#include <iostream>
+#include <ostream>
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(0) {
+	std::cout << "Called defalult constructor with name " << getName() << " and grade " << getGrade() << std::endl;
+}
+
+Bureaucrat::Bureaucrat(int grade) : _name("Default") { 
+	
+	try
+	{
+		setGrade(grade);
+	}
+	catch (Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << "Constructions " << getName() << " failed" << e.what() << " Grade not set to 1" << getGrade() << std::endl;
+		setGrade(1);
+	}
+	catch (Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << "Constructions " << getName() << " failed" << e.what() << " Grade not set to 150" << getGrade() << std::endl; 
+		setGrade(150);
+	}
+}
+
+Bureaucrat::Bureaucrat(std::string name) : _name(name) {
+	std::cout << "Called constructor with name " << _name << std::endl;
+}
--- CPP_Piscine/module_05/ex00/Bureaucrat.hpp
+++ CPP_Piscine/module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,54 @@
 /*   By: dkaratae <dkaratae@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 18:42:21 by dkaratae          #+#    #+#             */
-/*   Updated: 2023/06/13 18:42:22 by dkaratae         ###   ########.fr       */
+/*   Updated: 2023/10/23 16:09:58 by dkaratae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <exception>
+#include <ostream>
+#include <string>
+#include <iostream>
+
+class Bureaucrat {
+
+	private:
+		const std::string _name;
+		int _grade;
+	
+	public:
+		Bureaucrat();
+		Bureaucrat(int grade);
+		Bureaucrat(std::string name);
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat &operator == (const Bureaucrat &other);
+		~Bureaucrat();
+
+		//Setters
+		void setName(std::string name);
+		void setGrade(int grade);
+
+		//Getters
+		std::string getName();
+		int getGrade();
+		
+		//Metods
+		void incGrade();
+		void decGrade();
+		
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+		
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+};
+	//Overload operator <<
+	std::ostream &operator << (std::ostream &os, const Bureaucrat &print);
+#endif;
